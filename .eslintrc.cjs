e010a8e90e5fe8323a73e6b6e77a35fa83fde1f4
wencwcoder <1748994690@qq.com>
2022-09-03 17:38:23 +0800

    feat(init): 新增部分项目配置。

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  // 根目录
+  root: true,
+  // 环境
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  // 定义全局变量: readonly | writable
+  // globals: {
+  //   Promise: "readonly",
+  // },
+  // 扩展配置
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  // 解析器
+  // parser: "esprima",
+  // 解析器选项
+  parserOptions: {
+    ecmaVersion: "latest",
+    // ecmaVersion: 12, // 2021
+    // sourceType: "module",
+    // ecmaFeatures: {
+    //   jsx: true,
+    // },
+  },
+  // 插件
+  // plugins: [],
+  rules: {},
+};
